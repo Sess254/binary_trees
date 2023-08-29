270f7c2eefc078298eac7daac137a897e3a60732
Sess254 <wamaritemoses@gmail.com>
2023-08-29 18:44:20 +0300

    depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,29 @@
+/*
+ * File: 10-binary_tree_depth.c
+ * Auth: Sess254
+ */
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of the binary tree
+ * @tree: pointer to the node to measure the depth of
+ * Return: the depth, 0 if tree is NULL
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree->parent == NULL)
+		{
+			return (0);
+		}
+
+		return (1 + binary_tree_depth(tree->parent));
+	}
+}
