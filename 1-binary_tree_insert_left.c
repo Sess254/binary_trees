e01e3c4dc6ec18abfe7191b71e2be982800f4249
Sess254 <wamaritemoses@gmail.com>
2023-08-29 13:57:52 +0300

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,6 +19,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	if (parent != NULL)
 	{
 		if (parent->left != NULL)
