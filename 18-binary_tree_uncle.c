e885bfaf570b4cba9629b8db045d90801fcb3046
Sess254 <wamaritemoses@gmail.com>
2023-08-31 14:40:23 +0300

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,13 +12,10 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
-	binary_tree_t *grandparent = node->parent->parent;
-
-	if (node == NULL || parent == NULL || grandparent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	if (grandparent->left == parent)
-		return (grandparent->right);
-	return (grandparent->left);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
