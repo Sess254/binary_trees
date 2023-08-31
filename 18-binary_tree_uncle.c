9668c83f768ba6247e8704576310b60ee17a9c3d
Sess254 <wamaritemoses@gmail.com>
2023-08-31 14:21:40 +0300

    is unlce

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,7 +13,7 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *parent = node->parent;
-	binary_tree_t *grandparent = parent->parent;
+	binary_tree_t *grandparent = node->parent->parent;
 
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
